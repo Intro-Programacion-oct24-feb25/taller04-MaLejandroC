21636e3167adbf9e19b06fab0f42f81d7fe46233
Michael Carrion <macarrion44@utpl.edu.ec>
2024-11-01 01:00:42 -0500

    PROBLEMAS RESUELTOS

---
+++ problema01/Problema01/src/problema01/Problema01.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema01;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Alejandro
+ */
+public class Problema01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner area = new Scanner(System.in);
+        
+        double base;
+        double altura;
+        double formula;
+        
+        System.out.println("Ingrese la base del triangulo: ");
+        base = area.nextDouble();
+        System.out.println("Ingrese la altura del triangulo: ");
+        altura = area.nextDouble();
+        
+        formula = (base * altura)/2;
+        
+        System.out.println("El area del triangulo es: "+formula);
+        
+    }
+    
+}
